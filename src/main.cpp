96e051c32640e490f25a43232d296abfc87b1b6c
jayzhang <jayzhang422@gmail.com>
2026-04-26 14:54:07 +0800

    inRange阈值算法提示

--- src/main.cpp
+++ src/main.cpp
@@ -9,45 +9,127 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/videoio.hpp>
+#include "string"
+const int max_value_H = 360/2;
+const int max_value_S = 255;
+const int max_value_V = 255;
 
- 
-const char* trackbar_type = "Type: \n 0: Binary \n 1: Binary Inverted \n 2: Truncate \n 3: To Zero \n 4: To Zero Inverted";
-const char* trackbar_value = "Value";
-cv::Mat src,dst,src_gray;
+const std::string window_image_name = "Lego image";
+const std::string window_detection_name = "object detection";
 
-int threshold_value = 0 ;
-int threshold_type = 3;
+int low_H = 0 , low_S = 0 , low_V = 0;
+int high_H = max_value_H , high_S = max_value_S , high_V = max_value_V;
 
-int const max_value = 255 ;
-int const max_binary_value = 255;
-int const max_type = 4;
 
-static void Threshold_Demo(int pos , void* userdata)
+static void on_low_H_trackbar(int pos,void* userdata)
 {
     (void)pos;
     (void)userdata;
+    low_H = cv::min(low_H,high_H-1);
+    cv::setTrackbarPos("Low_H", window_detection_name, low_H);
 
-    cv::threshold(src_gray, dst, threshold_value, max_binary_value, threshold_type);
-    cv::imshow("Threshold image",dst);
+
 
 }
+   
+static void on_low_S_trackbar(int pos,void* userdata)
+{
+    (void)pos;
+    (void)userdata;
+    low_S = cv::min(low_S,high_S-1);
+    cv::setTrackbarPos("Low_S", window_detection_name, low_S);
     
+
+
+}
+
+static void on_low_V_trackbar(int pos,void* userdata)
+{
+    (void)pos;
+    (void)userdata;
+    low_V = cv::min(low_V,high_V-1);
+    cv::setTrackbarPos("Low_V", window_detection_name, low_V);
+    
+
+
+}
+
+static void on_high_H_trackbar(int pos,void* userdata)
+{
+    (void)pos;
+    (void)userdata;
+    high_H = cv::max(high_H,low_H+1);
+    cv::setTrackbarPos("High_H", window_detection_name, high_H);
+    
+
+
+}
+
+static void on_high_S_trackbar(int pos,void* userdata)
+{
+    (void)pos;
+    (void)userdata;
+    high_S = cv::max(high_S,low_S+1);   
+    cv::setTrackbarPos("High_S", window_detection_name, high_S);
+    
+
+
+}
+
+
+static void on_high_V_trackbar(int pos,void* userdata)
+{
+    (void)pos;
+    (void)userdata;
+    high_V = cv::max(high_V,low_V+1);
+    cv::setTrackbarPos("High_V", window_detection_name, high_V);
+    
+
+}
+
+
+
 int main()
 {
-    src = cv::imread("/home/jayzhang/VisionProject/data/test.jpg");
-    if(src.empty())
+
+    cv::Mat lego = cv::imread("/home/jayzhang/VisionProject/data/Lego.jpg");
+
+    if(lego.empty())
     {
-        std::cout<<"could not open the file"<<std::endl;
-        return -1;   
+        std::cout<<"could not open the image"<<std::endl;
+        return -1;
     }
 
-    cv::cvtColor(src, src_gray, cv::COLOR_BGR2GRAY);
+    cv::namedWindow(window_image_name);
+    cv::namedWindow(window_detection_name);
 
-    cv::namedWindow("Threshold image");
-
-    cv::createTrackbar(trackbar_type, "Threshold image", &threshold_type, max_type,Threshold_Demo);
-    cv::createTrackbar(trackbar_value, "Threshold image", &threshold_value, max_value,Threshold_Demo);
-    Threshold_Demo( 0, 0 );
-    cv::waitKey(0);
+    cv::createTrackbar("Low_H", window_detection_name, &low_H, max_value_H,on_low_H_trackbar);
+    cv::createTrackbar("Low_S", window_detection_name, &low_S, max_value_S,on_low_S_trackbar);
+    cv::createTrackbar("Low_V", window_detection_name, &low_V, max_value_V,on_low_V_trackbar);
+    cv::createTrackbar("High_H", window_detection_name, &high_H, max_value_H,on_high_H_trackbar);
+    cv::createTrackbar("High_S", window_detection_name, &high_S, max_value_S,on_high_S_trackbar);
+    cv::createTrackbar("High_V", window_detection_name, &high_V, max_value_V,on_high_V_trackbar);
+    on_high_H_trackbar(0, 0);
+    on_high_S_trackbar(0, 0);
+    on_high_V_trackbar(0, 0);
+    on_low_H_trackbar(0, 0);
+    on_low_S_trackbar(0, 0);
+    on_low_V_trackbar(0, 0);
+    cv::Mat image_HSV,dst;
+    cv::cvtColor(lego, image_HSV, cv::COLOR_BGR2HSV);
+   while (true) {
+       
+        cv::inRange(image_HSV, cv::Scalar(low_H, low_S, low_V), cv::Scalar(high_H, high_S, high_V), dst);
+        
+        
+        cv::imshow(window_image_name, lego);
+        cv::imshow(window_detection_name, dst);
+   
+        
+        if (cv::waitKey(30) == 27) {
+            break;
+        }
+    }
+    
     
 }
